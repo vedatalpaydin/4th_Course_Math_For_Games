5299fac470ea8859bd6bb0ca2dfd97a8db38595e
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-02-28 12:18:30 +0300

    Bits,Bytes and Binary

---
+++ Tutorial_1_Bitwise_Operations/Assets/SetBits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBits : MonoBehaviour
+{
+    private int bSequence = 8+1+2;
+    void Start()
+    {
+        Debug.Log(Convert.ToString(bSequence,2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
