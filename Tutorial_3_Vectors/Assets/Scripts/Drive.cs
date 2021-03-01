ea728cf1e1a38e85457bb8c0ba67a6c16ef9a599
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-03-01 21:53:20 +0300

    Vector Normal Form

---
+++ Tutorial_3_Vectors/Assets/Scripts/Drive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    private float speed = 5;
+    public GameObject fuel;
+    private Vector3 dir;
+    private float stoppingDist = 0.1f;
+
+    private void Start()
+    {
+        dir = fuel.transform.position - transform.position;
+        Coords dirNormal = HolisticMath.GetNormal(new Coords(dir));
+        dir = dirNormal.ToVector();
+    }
+
+    void Update()
+    {
+        if (HolisticMath.Distance(new Coords(transform.position), new Coords(fuel.transform.position)) > stoppingDist)
+            transform.position += dir * speed * Time.deltaTime;
+    }
+}
---
+++ Tutorial_3_Vectors/Assets/Scripts/HolisticMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolisticMath
+{
+    static public Coords GetNormal(Coords vector)
+    {
+        float lenght = Distance(new Coords(0, 0, 0), vector);
+        vector.x /= lenght;
+        vector.y /= lenght;
+        vector.z /= lenght;
+
+        return vector;
+    }
+
+    static public float Distance(Coords point1, Coords point2)
+    {
+        float diffSquared = Square(point1.x - point2.x) + Square(point1.y - point2.y)+Square(point1.z - point2.z);
+        float squareRoot = Mathf.Sqrt(diffSquared);
+        return squareRoot;
+    }
+
+    static public float Square(float value)
+    {
+        return value * value;
+    }
+}
