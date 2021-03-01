416599a69de49633a85d9ebd01d3d692b161c8a1
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-03-01 10:04:03 +0300

    Calculating Distance Part 1

--- Tutorial_2.1_Practice_Location_2D_Points_on_a_Cartesian_Plane/Assets/ObjectManager.cs
+++ Tutorial_2.1_Practice_Location_2D_Points_on_a_Cartesian_Plane/Assets/ObjectManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class ObjectManager : MonoBehaviour
 {
     public GameObject objPrefab;
+
+    public Vector3 objPos;
     // Start is called before the first frame update
     void Start()
     {
         GameObject obj = Instantiate(objPrefab, new Vector3(Random.Range(-100, 100), Random.Range(-100, 100), objPrefab.transform.position.z),
             Quaternion.identity);
+        objPos = obj.transform.position;
     }
 
     // Update is called once per frame
---
+++ Tutorial_2.1_Practice_Location_2D_Points_on_a_Cartesian_Plane/Assets/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject tank;
+
+    public GameObject fuel;
+
+    public Text tankPos;
+
+    public Text fuelPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tankPos.text = tank.transform.position+"";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
