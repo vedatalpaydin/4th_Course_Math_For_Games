704982a89c7dbbad4ae9f134138d967f8bef4e9f
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-03-02 19:58:16 +0300

    Moving from 2d to 3d

---
+++ Tutorial_3.2_Moving_From_2D_to_3D/Assets/Fly.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+    public float speed = 1.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") *speed;
+        float y = Input.GetAxis("VerticalY") *speed;
+        float z = Input.GetAxis("Vertical") *speed;
+       
+        transform.Translate(x,y,z);
+        
+    }
+}
