3cc5fdfbe2176f6ab844a5e8820e17f2a8ed8237
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-03-02 23:42:32 +0300

    40.Lİnes

---
+++ Tutorial_4_Intersections/Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform start;
+
+    public Transform end;
+
+    private Line line;
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = new Line(new Coords(start.position),new Coords(end.position));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = line.GetPointAt(Time.time).ToVector();
+    }
+}
