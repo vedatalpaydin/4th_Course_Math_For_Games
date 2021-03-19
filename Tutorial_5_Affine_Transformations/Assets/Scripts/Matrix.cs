419a585c3e751a63d660cb60cc987fa0bd1e4435
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-03-19 13:30:01 +0300

    tutorial 5

---
+++ Tutorial_5_Affine_Transformations/Assets/Scripts/Matrix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+public class Matrix
+{
+    private float[] values;
+    private int rows;
+    private int cols;
+
+    public Matrix(int r, int c, float[] v)
+    {
+        rows = r;
+        cols = c;
+        values = new float[rows * cols];
+        Array.Copy(v, values, rows * cols);
+    }
+
+    public override string ToString()
+    {
+        string matrix = "";
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                matrix += values[r * cols + c] + " ";
+            }
+
+            matrix += "\n";
+        }
+
+        return matrix;
+    }
+    static public Matrix operator+(Matrix a,Matrix b)
+    {
+        if (a.rows != b.rows || a.cols != b.cols)
+            return null;
+        Matrix result = new Matrix(a.rows, a.cols, a.values);
+        int length = a.rows * a.cols;
+        for (int i = 0; i < length; i++)
+        {
+            result.values[i] += b.values[i];
+        }
+
+        return result;
+    }
+}
